5855dce7fe90f506bac7582d483c8479d0c505b8
edgarito <roff7777@gmail.com>
2021-02-16 21:13:43 -0600

    agrego salida estandard para los dos end point

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.get('/colaborators', function (req, res) {
-    console.log(req.params)
-    res.send('Saludos desde express');
+app.get('/colaborators/:userId', (req, res) => {
+    return res.send(
+      {
+        'last_sync': 'Fecha de la última sincronización en dd/mm/yyyy hh:mm:ss', 
+        'editors' : [
+            'Wilson Moss',
+            'Andy Smidth'
+        ],
+        'writers' : [
+            'Ed Brubaker',
+            'Ryan North'
+        ],
+        'colorists' : [
+            'Rico Renzi'
+        ]
+      }
+    );
 });
 
+app.get('/characters/:userId', (req, res) => {
+    return res.send(
+        {
+            'last_sync': 'Fecha de la última sincronización en dd/mm/yyyy hh:mm:ss', 
+            'characters' : [
+                {
+                    'character': 'Squirrel Girl',
+                    'comics' : ['The Unbeateable Squirrel Girl (2015) #38','The Unbeateable Squirrel Girl(2015) #39']
+                },
+                {
+                    'character': 'Jocasta',
+                    'comics' :['Tony Stark: Iron Man (2018) #2','Tony Stark: Iron Man (2018) #3']
+                },
+            ]
+        }
+    )
+});
 
 
 app.listen(3000, () => {
